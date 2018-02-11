95f987d2ba92c6018900f4a8cd6f829f797a76a3
August Conwell <august.conwell@gmail.com>
2018-02-11 11:22:55 -0500

    Roll score derived from state

--- src/reducers/bowlingReducer.js
+++ src/reducers/bowlingReducer.js
@@ -8,13 +8,13 @@ export default function bowling(state = initialState, action) {
             const frameIndex = action.frameIndex;
             const targetFrame = state.frames[frameIndex];
             const rollIndex = targetFrame.filter(score => score !== 0 && score).length;
-            const updatedFrame = [...targetFrame.slice(0, rollIndex), action.score, ...targetFrame.slice(rollIndex + 1)];
+            const updatedFrame = [...targetFrame.slice(0, rollIndex), state.mostRecentRollScore, ...targetFrame.slice(rollIndex + 1)];
             const frames = [...state.frames.slice(0, frameIndex), updatedFrame, ...state.frames.slice(frameIndex + 1)];
             return Object.assign({}, state, { frames });
 
         case KNOCK_DOWN_SOME_PINS:
-            const remainingPins = 10 - (state.frames[currentFrameIndex][0] || 0);
-            const numberOfPinsKockedDown = Math.floor(action.randomFloat * (remainingPins));
+            const remainingPins = 10 - (state.frames[action.currentFrameIndex][0] || 0);
+            const numberOfPinsKockedDown = Math.floor(action.randomFloat * (remainingPins + 1));
             return Object.assign({}, state, { mostRecentRollScore: numberOfPinsKockedDown })
         
         default:
