9a44b867336dc13d79ba43fb2cebfd46d1a27a46
August Conwell <august.conwell@gmail.com>
2018-02-11 13:49:59 -0500

    Simplify rollIndex

--- src/reducers/bowlingReducer.js
+++ src/reducers/bowlingReducer.js
@@ -7,7 +7,7 @@ export default function bowling(state = initialState, action) {
         case APPLY_ROLL_SCORE:
             const frameIndex = action.frameIndex;
             const targetFrame = state.frames[frameIndex];
-            const rollIndex = targetFrame.filter(score => score !== 0 && score).length;
+            const rollIndex = targetFrame.filter(score => score !== undefined).length; // Count the rolls where the score is not undefined
             const updatedFrame = [...targetFrame.slice(0, rollIndex), state.mostRecentRollScore, ...targetFrame.slice(rollIndex + 1)];
             const frames = [...state.frames.slice(0, frameIndex), updatedFrame, ...state.frames.slice(frameIndex + 1)];
             return Object.assign({}, state, { frames });
