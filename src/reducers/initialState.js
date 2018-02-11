043ff1cc8e120458a159e98dee86859b95ae3cc1
August Conwell <august.conwell@gmail.com>
2018-02-11 11:23:26 -0500

    Removed unnecessary fields - currentFrameIndex, currentRollIndex

--- src/reducers/initialState.js
+++ src/reducers/initialState.js
@@ -1,7 +1,5 @@
 export default {
 	mostRecentRollScore: undefined,
-	currentFrameIndex: 0,
-	currentRollIndex: 0,
 	frames: [
 		[undefined, undefined, undefined], 
 		[undefined, undefined, undefined],
