a994349a238f0f09f555dd5e2f4bbe1b47677dd8
August Conwell <august.conwell@gmail.com>
2018-02-11 18:09:51 -0500

    Added spare/strike action

--- src/actions/bowlingActions.js
+++ src/actions/bowlingActions.js
@@ -1,10 +1,17 @@
-import {APPLY_ROLL_SCORE, KNOCK_DOWN_SOME_PINS } from './actionTypes';
+import {APPLY_ROLL_SCORE, KNOCK_DOWN_SOME_PINS, APPLY_SPARE_AND_STRIKE_EXTRAS } from './actionTypes';
 
 export function applyRollScore(frameIndex) {
 	return {
 		type: APPLY_ROLL_SCORE, 
         frameIndex,
 	}
+}
+
+export function applySpareAndStrikeExtras(frameIndex) {
+    return {
+        type: APPLY_SPARE_AND_STRIKE_EXTRAS,
+        frameIndex,
+    }
 }
 
 export function knockDownSomePins(randomFloat, currentFrameIndex) {
--- src/actions/bowlingActions.test.js
+++ src/actions/bowlingActions.test.js
@@ -1,19 +1,26 @@
-import * as actions from './bowlingActions'
-import { APPLY_ROLL_SCORE, KNOCK_DOWN_SOME_PINS } from './actionTypes';
+import * as actions from `./bowlingActions`
+import { APPLY_ROLL_SCORE, KNOCK_DOWN_SOME_PINS, APPLY_SPARE_AND_STRIKE_EXTRAS } from `./actionTypes`;
 
-describe('actions', () => {
-    it('should create an action apply a score to a roll', () => {
-        const score = 5;
+describe(`actions`, () => {
+    it(`should create an action to apply a score to a roll`, () => {
         const frameIndex = 4;
         const expectedAction = {
             type: APPLY_ROLL_SCORE,
-            score,
             frameIndex
         }
-        expect(actions.applyRollScore(5, 4)).toEqual(expectedAction)
+        expect(actions.applyRollScore(4)).toEqual(expectedAction)
     });
 
-    it('should create an action knock down some pins', () => {
+    it(`should create an action to apply a score to a previous frame`, () => {
+        const frameIndex = 4;
+        const expectedAction = {
+            type: APPLY_SPARE_AND_STRIKE_EXTRAS,
+            frameIndex
+        }
+        expect(actions.applyRollScore(4)).toEqual(expectedAction)
+    });
+
+    it(`should create an action knock down some pins`, () => {
         const randomFloat = 0.4;
         const expectedAction = {
             type: KNOCK_DOWN_SOME_PINS,
