52c943957496c8bab53cf3f8d1af2202df92c909
August Conwell <august.conwell@gmail.com>
2018-02-11 23:01:17 -0500

    Running and absolute totals

--- src/components/scoreboard/ScoreboardContainer.js
+++ src/components/scoreboard/ScoreboardContainer.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import Frame from './Frame';
 import Scoreboard from './Scoreboard';
-import getFrameTotal from './getFrameTotal';
+import Frame from './Frame';
+import Total from './Total';
+import getCurrentFrame from '../roll/getCurrentFrame';
+import { getRunningTotal, getAbsoluteTotal } from './totals';
 
 class ScoreboardContainer extends Component {
     constructor(props) {
         super(props);
 
         this.renderFrames = this.renderFrames.bind(this);
+        this.runningTotal = this.runningTotal.bind(this);
     }
 
     renderFrames() {
@@ -18,21 +21,39 @@ class ScoreboardContainer extends Component {
                 index={index}
                 score1={scores[0] !== undefined && scores[0]}
                 score2={scores[1] !== undefined && scores[1]}
-                total={getFrameTotal(scores)}
+                total={this.runningTotal(index)}
             />
         ))
+    }
+
+    runningTotal(index) {
+        return index < this.props.currentFrameIndex && getRunningTotal(this.props.frames, index);
     }
 
     render() {
         return (
             <Scoreboard>
-                {this.renderFrames()}
+                <div className="clr">
+                    {this.renderFrames()}
+                </div>
+                <Total total={
+                    getAbsoluteTotal(
+                        this.props.frames,
+                        this.props.spareAndStrikeExtras,
+                        this.props.currentFrameIndex
+                    )
+                } />
             </Scoreboard>
         )
     }
 }
 
-const mapStateToProps = ({ frames, currentFrameIndex }) => ({ frames, currentFrameIndex });
+const mapStateToProps = ({ frames, spareAndStrikeExtras }) => (
+    {
+        frames,
+        currentFrameIndex: getCurrentFrame(frames),
+        spareAndStrikeExtras
+    });
 
 export default connect(mapStateToProps)(ScoreboardContainer)
 
