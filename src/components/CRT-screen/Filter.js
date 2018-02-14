58e82c4d51bc93258c7febc1df7b212c9aa02631
August Conwell <august.conwell@gmail.com>
2018-02-14 00:25:55 -0500

    Blur and displacement filters

---
+++ src/components/CRT-screen/Filter.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+import crtpng from '../../assets/crt.png';
+
+export const DisplacementFilter = (props) => (
+    <filter id={props.id}>
+        <circle id="circle1" cx="100" cy="100" r="70" />
+        <feImage
+            xlinkHref={crtpng}
+            result="fisheye"
+            height="100%"
+            width="100%"
+            x="0"
+            y="0"
+        />
+        <feDisplacementMap
+            in2="fisheye"
+            in="SourceGraphic"
+            scale="80"
+            xChannelSelector="R"
+            yChannelSelector="A"
+        />
+    </filter>
+);
+
+export const BlurFilter = (props) => (
+    <filter id={props.id}>
+        <feGaussianBlur in="SourceGraphic" stdDeviation="5" />
+    </filter>
+)
